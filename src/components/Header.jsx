7195adf45a53f51c780b5ffc5b77017889b3db30
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-29 20:43:28 -0600

    added styling to elements in Header and Main. put Footer styling in App.css for practice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 
 export default function Header() {
     return (
-        <header>
-        <h1 id="title">Satre's List</h1>
-        <h2>Better-Dressed People</h2>
+        <header style={{padding:'1rem 2rem'}}>
+        <h1 id="title" style={{color:'tomato', fontSize:'4rem'}}>Satre's List</h1>
+        <h2 style={{fontSize:'2.5rem'}}>Better-Dressed People</h2>
         <nav aria-label="Main Navigation" role="navigation">
-          <ul>
-            <li><a href="...">Women's</a></li>
-            <li><a href="...">Men's</a></li>
-            <li><a href="...">On the Street</a></li>
-            <li><a href="...">The Catwalk</a></li>
-            <li><a href="...">AdWatch</a></li>
-            <li><a href="...">About</a></li>
+          <ul style={{padding: 0, listStyleType: 'none', display:'flex', justifyContent: 'space-between', textDecoration: 'underline'}}>
+            <li><a style={{color:'gray'}} href="...">Women's</a></li>
+            <li><a style={{color:'gray'}} href="...">Men's</a></li>
+            <li><a style={{color:'gray'}} href="...">On the Street</a></li>
+            <li><a style={{color:'gray'}} href="...">The Catwalk</a></li>
+            <li><a style={{color:'gray'}} href="...">AdWatch</a></li>
+            <li><a style={{color:'gray'}} href="...">About</a></li>
           </ul>
         </nav>
       </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,38 +2,39 @@ import image from "../images/image.png";
 import image2 from "../images/image.png";
 
 export default function Main() {
+
   return (
-    <main>
+    <main style={{padding:'1rem 2rem'}}>
       <section>
-        <article>
-          <time dateTime="2015-08-07">11/12/20</time>
-          <h2>On the Street in Brooklyn</h2>
-          <img src={image} alt="Girl in orange dress" />
-          <p>
+        <article style = {{marginBottom: '4rem', borderBottom: '1px solid gray', paddingBottom: '2rem'}}>
+          <time style={{fontSize:'2.5rem', color:'lightgray'}} dateTime="2015-08-07">11/12/20</time>
+          <h2 style={{fontSize:'3rem'}}>On the Street in Brooklyn</h2>
+          <img src={image} width='100%' alt="Girl in orange dress" />
+          <p style={{fontSize:'1.5rem'}}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius cumque
             porro, voluptatem veritatis consequuntur laborum nihil excepturi
             suscipit adipisci harum hic quae maiores voluptate facere ab minus
             doloribus magnam atque!
           </p>
           <aside>
-            <h4>Continues...</h4>
+            <h4 style={{color:'tomato', fontWeight:'bold', textAlign:'right', fontSize:'1.5rem'}}>Continues...</h4>
           </aside>
         </article>
       </section>
 
       <section>
-        <article>
-          <time dateTime="2015-08-07">11/11/20</time>
-          <h2>Vintage in Vogue</h2>
-          <img src={image2} alt="Fancy young people" />
-          <p>
+        <article style = {{marginBottom: '4rem', borderBottom: '1px solid gray', paddingBottom: '2rem'}}>
+          <time style={{fontSize:'2.5rem', color:'lightgray'}} dateTime="2015-08-07">11/11/20</time>
+          <h2 style={{fontSize:'3rem'}}>Vintage in Vogue</h2>
+          <img src={image2} width='100%' alt="Fancy young people" />
+          <p style={{fontSize:'1.5rem'}}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
             quo magni magnam facere distinctio aliquam voluptatum assumenda eos
             nostrum. Neque ab ducimus facilis eos ipsam quod nemo possimus!
             Esse, ex?
           </p>
           <aside>
-            <h4>Continues ...</h4>
+            <h4 style={{color:'tomato', fontWeight:'bold', textAlign:'right', fontSize:'1.5rem'}}>Continues ...</h4>
           </aside>
         </article>
       </section>
